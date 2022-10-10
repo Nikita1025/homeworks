70f00114d227ddc1ca3900389735460e7cfd06ff
Nikita1025 <nikita1025@mail.ru>
2022-10-10 19:30:48 +0300

    Initial commit

---
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -0,0 +1,25 @@
+import {UserType} from "../HW8";
+
+type ActionType =
+    { type: 'sort'; payload: 'up' | 'down' }
+    | { type: 'check'; payload: number }
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+    switch (action.type) {
+        case 'sort': {
+            if(action.payload === 'up') {
+                return [...state.sort((a, b) => a.name.localeCompare(b.name))]
+            }
+            if(action.payload === 'down') {
+                return [...state.sort((a, b) => b.name.localeCompare(a.name))]
+            }
+            return state
+        }
+        case 'check': {
+            return state.filter(el => el.age > action.payload)
+
+        }
+        default:
+            return state
+    }
+}
