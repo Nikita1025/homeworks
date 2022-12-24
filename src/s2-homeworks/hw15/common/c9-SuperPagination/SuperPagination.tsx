010b22c358170ecd4c6f6801d0f87a94b1c44038
Nikita1025 <nikita1025@mail.ru>
2022-12-24 20:16:24 +0300

    HW15

--- src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
+++ src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
@@ -25,7 +25,7 @@ const SuperPagination: React.FC<SuperPaginationPropsType> = (
 
     const onChangeSelect = (event: ChangeEvent<HTMLSelectElement>) => {
         // пишет студент
-        onChange(1, +event.currentTarget.value)
+        onChange(page, +event.currentTarget.value)
     }
 
     return (
